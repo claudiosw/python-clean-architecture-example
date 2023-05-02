12dfead10b39e6eb215c87de9410c39df0577626
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-05-02 13:13:11 -0300

    feat: Added Create Profession Controller

---
+++ src/app/cli_memory/controllers/create_profession_controller.py
@@ -0,0 +1,31 @@
+"""Create Profession Controller Module"""
+
+
+from src.interactor.use_cases.create_profession import CreateProfessionUseCase
+from src.infra.repositories.profession_in_memory_repository \
+    import ProfessionInMemoryRepository
+from src.interactor.dtos.create_profession_dtos import CreateProfessionInputDto
+from src.app.cli_memory.interfaces.cli_memory_controller_interface \
+    import CliMemoryControllerInterface
+from ..presenters.create_profession_presenter import CreateProfessionPresenter
+from ..views.create_profession_view import CreateProfessionView
+
+
+class CreateProfessionController(CliMemoryControllerInterface):
+    """ Create Profession Controller Class
+    """
+    def _get_profession_info(self) -> CreateProfessionInputDto:
+        name = input("Enter the profession name:")
+        description = input("Enter the profession description:")
+        return CreateProfessionInputDto(name, description)
+
+    def execute(self):
+        """ Execute the create profession controller
+        """
+        repository = ProfessionInMemoryRepository()
+        presenter = CreateProfessionPresenter()
+        input_dto = self._get_profession_info()
+        use_case = CreateProfessionUseCase(presenter, repository)
+        result = use_case.execute(input_dto)
+        view = CreateProfessionView()
+        view.show(result)
