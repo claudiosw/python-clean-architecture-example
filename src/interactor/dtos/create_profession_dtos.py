e6f95662ec9130ad65381a963f19656c2c7b0554
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-05-06 16:15:48 -0300

    feat: Added conversion to Dict in CreateProfessionInputDto

--- src/interactor/dtos/create_profession_dtos.py
+++ src/interactor/dtos/create_profession_dtos.py
@@ -2,7 +2,7 @@
 """
 
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from src.domain.entities.profession import Profession
 
 
@@ -11,6 +11,11 @@ class CreateProfessionInputDto:
     """ Input Dto for create profession """
     name: str
     description: str
+
+    def to_dict(self):
+        """ Convert data into dictionary
+        """
+        return asdict(self)
 
 
 @dataclass
