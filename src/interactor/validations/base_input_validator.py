471cec3e02cd72cf177bfe4b72d91cff4befcf41
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-05-07 11:47:14 -0300

    feat: Added BaseInputValidator to validate data with Cerberus

---
+++ src/interactor/validations/base_input_validator.py
@@ -0,0 +1,32 @@
+""" This module provides the base class BaseInputValidator for input validation
+"""
+
+
+from typing import Dict
+from cerberus import Validator  # type: ignore
+
+
+class BaseInputValidator:
+    """ This class provides the base class for input validation
+    """
+    def __init__(self, data: Dict[str, str]):
+        self.data = data
+        self.errors: Dict = {}
+
+    def verify(self, schema: Dict) -> None:
+        """ Validates the input data against the provided schema
+        :param schema: The schema to validate against
+        :return: None
+        :raises ValueError: If the input data is invalid.
+        """
+        validator = Validator(schema)
+        if not validator.validate(self.data):
+            self.errors = validator.errors
+            self._raise_validation_error()
+
+    def _raise_validation_error(self):
+        error_messages = []
+        for field, messages in self.errors.items():
+            for message in messages:
+                error_messages.append(f"{field.capitalize()}: {message}")
+        raise ValueError("\n{}".join(error_messages))
---
+++ src/interactor/validations/base_input_validator_test.py
@@ -0,0 +1,63 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+
+import pytest
+from typing import Dict
+from src.interactor.validations.base_input_validator import BaseInputValidator
+
+
+class BaseValidator(BaseInputValidator):
+    def __init__(self, data: Dict):
+        super().__init__(data)
+        self.schema = {
+                "name": {
+                    "type": "string",
+                    "minlength": 3,
+                    "maxlength": 10,
+                    "required": True,
+                    "empty": False
+                }
+        }
+
+    def validate(self):
+        super().verify(self.schema)
+
+
+def test_base_validator_with_valid_data():
+    data = {'name': 'test'}
+    validator = BaseValidator(data)
+    validator.validate()
+
+
+def test_base_validator_with_small_data():
+    data = {'name': 'a'}
+    validator = BaseValidator(data)
+    with pytest.raises(ValueError) as exception_info:
+        validator.validate()
+    assert str(exception_info.value) == "Name: min length is 3"
+
+
+def test_base_validator_with_long_data():
+    data = {'name': 'this is a long name'}
+    validator = BaseValidator(data)
+    with pytest.raises(ValueError) as exception_info:
+        validator.validate()
+    assert str(exception_info.value) == "Name: max length is 10"
+
+
+def test_base_validator_with_empty_data():
+    data = {'name': ''}
+    validator = BaseValidator(data)
+    with pytest.raises(ValueError) as exception_info:
+        validator.validate()
+    assert str(exception_info.value) == "Name: empty values not allowed"
+
+
+def test_base_validator_without_required_data():
+    data = {}
+    validator = BaseValidator(data)
+    with pytest.raises(ValueError) as exception_info:
+        validator.validate()
+    assert str(exception_info.value) == "Name: required field"
