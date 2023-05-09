42408ff3bb057e5a6793338692b35f7967a458f6
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-05-09 11:09:46 -0300

    feat: Added logging to the CreateProfessionController

--- cli_memory_process_handler.py
+++ cli_memory_process_handler.py
@@ -2,19 +2,24 @@
 """
 
 
+from typing import Dict
 from src.app.cli_memory.interfaces.cli_memory_controller_interface \
     import CliMemoryControllerInterface
 from src.app.cli_memory.controllers.exit_controller \
     import ExitController
 from src.app.cli_memory.controllers.create_profession_controller \
     import CreateProfessionController
+from src.interactor.errors.error_classes import FieldValueNotPermittedException
+from src.interactor.interfaces.logger.logger import LoggerInterface
+from src.infra.loggers.logger_default import LoggerDefault
 
 
 class CliMemoryProcessHandler:
     """ The ProcessHandler for Cli that uses Memory repository
     """
-    def __init__(self):
-        self.options = {}
+    def __init__(self, logger: LoggerInterface) -> None:
+        self.logger = logger
+        self.options: Dict = {}
 
     def add_option(
         self,
@@ -45,13 +50,22 @@ class CliMemoryProcessHandler:
             choice = input("> ")
             option = self.options.get(choice)
             if option:
-                option.execute()
+                try:
+                    option.execute()
+                except (
+                    ValueError,
+                    FieldValueNotPermittedException
+                ) as exception:
+                    print(f'\nERROR: {str(exception)}\n')
+                    self.logger.log_error(str(exception))
             else:
                 print("Invalid choice.")
+                self.logger.log_info(f"Invalid user choice: {option}")
 
 
 if __name__ == "__main__":
-    process = CliMemoryProcessHandler()
+    logger_default = LoggerDefault()
+    process = CliMemoryProcessHandler(logger_default)
     process.add_option("0", ExitController())
-    process.add_option("1", CreateProfessionController())
+    process.add_option("1", CreateProfessionController(logger_default))
     process.execute()
--- src/app/cli_memory/controllers/create_profession_controller.py
+++ src/app/cli_memory/controllers/create_profession_controller.py
@@ -7,6 +7,7 @@ from src.infra.repositories.profession_in_memory_repository \
 from src.interactor.dtos.create_profession_dtos import CreateProfessionInputDto
 from src.app.cli_memory.interfaces.cli_memory_controller_interface \
     import CliMemoryControllerInterface
+from src.interactor.interfaces.logger.logger import LoggerInterface
 from ..presenters.create_profession_presenter import CreateProfessionPresenter
 from ..views.create_profession_view import CreateProfessionView
 
@@ -14,6 +15,9 @@ from ..views.create_profession_view import CreateProfessionView
 class CreateProfessionController(CliMemoryControllerInterface):
     """ Create Profession Controller Class
     """
+    def __init__(self, logger: LoggerInterface):
+        self.logger = logger
+
     def _get_profession_info(self) -> CreateProfessionInputDto:
         name = input("Enter the profession name:")
         description = input("Enter the profession description:")
@@ -25,7 +29,7 @@ class CreateProfessionController(CliMemoryControllerInterface):
         repository = ProfessionInMemoryRepository()
         presenter = CreateProfessionPresenter()
         input_dto = self._get_profession_info()
-        use_case = CreateProfessionUseCase(presenter, repository)
+        use_case = CreateProfessionUseCase(presenter, repository, self.logger)
         result = use_case.execute(input_dto)
         view = CreateProfessionView()
         view.show(result)
