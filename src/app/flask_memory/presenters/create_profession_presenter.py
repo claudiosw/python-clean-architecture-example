392b4eb4511522c7b7de29918708326c8427e31c
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-05-18 10:56:17 -0300

    feat: Added Flask Memory CreateProfessionPresenter

---
+++ src/app/flask_memory/presenters/create_profession_presenter.py
@@ -0,0 +1,24 @@
+""" Module for the CreateProfessionPresenter
+"""
+
+
+from typing import Dict
+from src.interactor.dtos.create_profession_dtos \
+    import CreateProfessionOutputDto
+from src.interactor.interfaces.presenters.create_profession_presenter \
+    import CreateProfessionPresenterInterface
+
+
+class CreateProfessionPresenter(CreateProfessionPresenterInterface):
+    """ Class for the CreateProfessionPresenter
+    """
+    def present(self, output_dto: CreateProfessionOutputDto) -> Dict:
+        """ Present the CreateProfession
+        :param output_dto: CreateProfessionOutputDto
+        :return: Dict
+        """
+        return {
+                    "profession_id": output_dto.profession.profession_id,
+                    "name": output_dto.profession.name,
+                    "description": output_dto.profession.description
+        }
