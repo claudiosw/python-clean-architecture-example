f75eebbeae701faa12e530a53cc73d9a9794ebd8
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-06-04 16:30:58 -0300

    improve: Corrected mock assertion calls

--- src/infra/loggers/logger_default_test.py
+++ src/infra/loggers/logger_default_test.py
@@ -11,29 +11,29 @@ def test_logger_default(mocker):
     mocker.patch.object(logging, 'debug')
     logger = LoggerDefault()
     logger.log_debug('testdebug')
-    assert logging.debug.call_once_with('testdebug')  # pylint: disable=E1101
+    logging.debug.assert_called_once_with('testdebug')  # pylint: disable=E1101
 
     mocker.patch.object(logging, 'info')
     logger = LoggerDefault()
     logger.log_info('testinfo')
-    assert logging.info.call_once_with('testinfo')  # pylint: disable=E1101
+    logging.info.assert_called_once_with('testinfo')  # pylint: disable=E1101
 
     mocker.patch.object(logging, 'warning')
     logger = LoggerDefault()
-    logger.log_warning('testwarn')
-    assert logging.warning.call_once_with('testwarn')  # pylint: disable=E1101
+    logger.log_warning('tstwarn')
+    logging.warning.assert_called_once_with('tstwarn')  # pylint: disable=E1101
 
     mocker.patch.object(logging, 'error')
     logger = LoggerDefault()
     logger.log_error('testerror')
-    assert logging.error.call_once_with('testerror')  # pylint: disable=E1101
+    logging.error.assert_called_once_with('testerror')  # pylint: disable=E1101
 
     mocker.patch.object(logging, 'critical')
     logger = LoggerDefault()
-    logger.log_critical('testcrit')
-    assert logging.critical.call_once_with('testcrit')  # pylint: disable=E1101
+    logger.log_critical('tcrit')
+    logging.critical.assert_called_once_with('tcrit')  # pylint: disable=E1101
 
     mocker.patch.object(logging, 'exception')
     logger = LoggerDefault()
-    logger.log_exception('testexc')
-    assert logging.exception.call_once_with('testexc')  # pylint: disable=E1101
+    logger.log_exception('texce')
+    logging.exception.assert_called_once_with('texce')  # pylint: disable=E1101
